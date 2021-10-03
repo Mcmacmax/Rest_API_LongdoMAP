edb1adf42dcf90c60ec0944ecc84b13f0dfe076e
Putisek Charoenrungruang <58245777+Mcmacmax@users.noreply.github.com>
2021-10-03 20:25:50 +0700

    Delete API_KEY.py

--- API_KEY.py
+++
@@ -1,6 +0,0 @@
-def API_Opendata():
-    API_Opendata = 'DFNYQ8zXpxxnz817MHpkxgPeGPG49LnB'
-    return (API_Opendata)
-def API_Longdomap():
-    API_LongdoMap = '13542e6a3467085019fd80b7d86b2d22'
-    return (API_LongdoMap)
